ca3018f61d3d4090661033534bdca4b890c31b98
em-henken-06 <emily.henken@slu.edu>
2024-01-22 17:32:59 -0600

    new

--- part2/script.js
+++ part2/script.js
@@ -1,5 +1,5 @@
-var map = L.map('map').setView([37.7, -122.4], 10);
-L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map = L.map('map').setView([38.635041,-90.237474], 12);
+ L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
